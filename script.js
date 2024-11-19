e2ac43915afb405e75488960e9ad55ad3d9d5471
dimitri3 <157725336+dimitri3-ctr@users.noreply.github.com>
2024-11-19 13:23:49 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+mouseClickMethod(select_char);
+getWidth();
+getHeight();
+var chrom = new Text("Chrom", "30pt Arial");
+var x = getWidth() / 2;
+var y = getWidth() / 2;
+chrom.setPosition(x, y);
+chrom.setColor(Color.blue)
