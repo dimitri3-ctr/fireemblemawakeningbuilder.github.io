3d0e78742857a08950ccd508e458d7611071e6a4
dimitri3 <157725336+dimitri3-ctr@users.noreply.github.com>
2024-11-21 13:46:52 -0600

    Update characters.js

--- database/characters.js
+++ database/characters.js
@@ -1,3 +1,8 @@
 export default {
   "chrom": {
-  
+    "base_class": "lord",
+    "available_classes": [ "lord", "cavalier", "archer", "great lord", "paladin", "great knight", "sniper", "bow knight" ],
+  },
+  "lissa": {
+    "base_class": "cleric",
+    "available_classes": [ "cleric", "pegasus knight", "troubadour", "
