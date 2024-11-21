6911fd9d7b1469cbeff3761e3d95928058782880
dimitri3 <157725336+dimitri3-ctr@users.noreply.github.com>
2024-11-21 14:19:06 -0600

    Update characters.js

--- database/characters.js
+++ database/characters.js
@@ -1,8 +1,23 @@
 export default {
+  "avatarM": {
+    "name": "Male Avatar",
+    "starting_class": "tactician",
+    "available_classes": [ "tactician", "grandmaster", "cavalier", "paladin", "knight", "general", "great knight", "myrmidon", "swordmaster", "thief", "trickster", "assassin", "barbarian", "berserker", "fighter", "warrior", "mercenary", "hero", "archer", "sniper", "bow knight", "dark mage", "sorcerer", "mage", "sage", "dark knight", "priest", "war monk", "wyvern rider", "wyvern lord", "griffon rider" ],
+  },
   "chrom": {
-    "base_class": "lord",
-    "available_classes": [ "lord", "cavalier", "archer", "great lord", "paladin", "great knight", "sniper", "bow knight" ],
+    "name": "Chrom",
+    "starting_class": "lord",
+    "available_classes": [ "lord", "great lord", "cavalier", "paladin", "great knight", "archer", "sniper", "bow knight" ],
   },
   "lissa": {
-    "base_class": "cleric",
-    "available_classes": [ "cleric", "pegasus knight", "troubadour", "
+    "name": "Lissa",
+    "starting_class": "cleric",
+    "available_classes": [ "cleric", "war cleric", "sage", "pegasus knight", "falcon knight", "dark flier", "troubadour", "valkyrie" ],
+  },
+  "frederick": {
+    "name": "Frederick",
+    "starting_class": "great knight",
+    "available_classes": [ "cavalier", "paladin", "knight", "general", "great knight", "wyvern rider", "wyvern lord", "griffon rider" ],
+  },
+  
+    
