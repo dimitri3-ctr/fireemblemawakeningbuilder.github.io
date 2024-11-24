e263f167d3423854546e80ec8cea39ded9ab0a29
dimitri3 <157725336+dimitri3-ctr@users.noreply.github.com>
2024-11-24 17:36:50 -0600

    Update and rename skills.js to classes.js

---
+++ database/classes.js
@@ -0,0 +1,7 @@
+export default {
+  "lord": [ "Dual Strike+", "Charm" ],
+  "great lord": [ "Aether", "Rightful King" ],
+  "tactician": [ "Veteran", "Solidarity" ],
+  "grandmaster": [ "Ignis", "Rally Spectrum" ],
+}  
+  
--- database/skills.js
+++
@@ -1,9 +0,0 @@
-export default {
-  "dual_strike+": {
-    "name": "Dual Strike +",
-    "class": "Lord",
-    "class_lvl": 1,
-    "effect": "Adds 10% to Dual Strike activation",
-  },
-  
-  
