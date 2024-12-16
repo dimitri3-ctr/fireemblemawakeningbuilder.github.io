17dbcd50fa369b95a8ff8f4257d1336ec7f640dc
dimitri3 <157725336+dimitri3-ctr@users.noreply.github.com>
2024-12-16 11:09:41 -0600

    Update skills.js

--- database/skills.js
+++ database/skills.js
@@ -1,7 +1,9 @@
 export default {
-  "lord": [ "Dual Strike+", "Charm" ],
-  "great lord": [ "Aether", "Rightful King" ],
-  "tactician": [ "Veteran", "Solidarity" ],
-  "grandmaster": [ "Ignis", "Rally Spectrum" ],
+  "class_skills": {
+    "lord": [ "Dual Strike+", "Charm" ],
+    "great lord": [ "Aether", "Rightful King" ],
+    "tactician": [ "Veteran", "Solidarity" ],
+    "grandmaster": [ "Ignis", "Rally Spectrum" ],
+  }
 }  
   
