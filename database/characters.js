214b885a3d6eb41903e308960f9f068e931789b7
dimitri3 <157725336+dimitri3-ctr@users.noreply.github.com>
2024-12-20 11:03:50 -0600

    Update characters.js

--- database/characters.js
+++ database/characters.js
@@ -1,20 +1,24 @@
 export default {
   "avatarM": {
+    "base_id": 1,
     "name": "Male Avatar",
     "starting_class": "tactician",
     "available_classes": [ "tactician", "grandmaster", "cavalier", "paladin", "knight", "general", "great knight", "myrmidon", "swordmaster", "thief", "trickster", "assassin", "barbarian", "berserker", "fighter", "warrior", "mercenary", "hero", "archer", "sniper", "bow knight", "dark mage", "sorcerer", "mage", "sage", "dark knight", "priest", "war monk", "wyvern rider", "wyvern lord", "griffon rider" ],
   },
   "chrom": {
+    "base_id": 2,
     "name": "Chrom",
     "starting_class": "lord",
     "available_classes": [ "lord", "great lord", "cavalier", "paladin", "great knight", "archer", "sniper", "bow knight" ],
   },
   "lissa": {
+    "base_id": 3,
     "name": "Lissa",
     "starting_class": "cleric",
     "available_classes": [ "cleric", "war cleric", "sage", "pegasus knight", "falcon knight", "dark flier", "troubadour", "valkyrie" ],
   },
   "frederick": {
+    "base_id": 4,
     "name": "Frederick",
     "starting_class": "great knight",
     "available_classes": [ "cavalier", "paladin", "knight", "general", "great knight", "wyvern rider", "wyvern lord", "griffon rider" ],
