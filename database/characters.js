47131e07e7886a3f1363278107e6d6f6dde81ba0
dimitri3 <157725336+dimitri3-ctr@users.noreply.github.com>
2025-09-29 20:47:45 -0500

    Update characters.js

--- database/characters.js
+++ database/characters.js
@@ -3,25 +3,29 @@ const characters = {
     base_id: 1,
     name: "Male Avatar",
     starting_class: "tactician",
-    available_classes: [ "tactician", "grandmaster", "cavalier", "paladin", "knight", "general", "great knight", "myrmidon", "swordmaster", "thief", "trickster", "assassin", "barbarian", "berserker", "fighter", "warrior", "mercenary", "hero", "archer", "sniper", "bow knight", "dark mage", "sorcerer", "mage", "sage", "dark knight", "priest", "war monk", "wyvern rider", "wyvern lord", "griffon rider" ]
+    available_classes: [ "tactician", "grandmaster", "cavalier", "paladin", "knight", "general", "great knight", "myrmidon", "swordmaster", "thief", "trickster", "assassin", "barbarian", "berserker", "fighter", "warrior", "mercenary", "hero", "archer", "sniper", "bow knight", "dark mage", "sorcerer", "mage", "sage", "dark knight", "priest", "war monk", "wyvern rider", "wyvern lord", "griffon rider" ],
+    available_skills: [ "veteran", "solidarity", "ignis", "rally spectrum", "discipline", "outdoor fighter", "defender", "aegis", "defense+2", "indoor fighter", "rally defense", "pavise", "luna", "dual guard+", "avoid+10", "vantage", "astra", "swordfaire", "locktouch", "movement+1", "lucky seven", "acrobat", "lethality", "pass", "despoil", "gamble", "wrath", "axefaire", "HP+5", "zeal", "rally strength", "counter", "armsthrift", "patience", "sol", "axebreaker", "skill+2", "prescience", "hit rate+20", "bowfaire", "rally skill", "bowbreaker", "hex", "anathema", "vengeance", "tomebreaker", "magic+2", "focus", "rally magic", "tomefaire", "slow burn", "lifetaker", "miracle", "healtouch", "rally luck", "renewal", "strength+2", "tantivy", "quick burn", "swordbreaker", "deliverer", "lancebreaker"]        
   },
   chrom: {
     base_id: 2,
     name: "Chrom",
     starting_class: "lord",
-    available_classes: [ "lord", "great lord", "cavalier", "paladin", "great knight", "archer", "sniper", "bow knight" ]
+    available_classes: [ "lord", "great lord", "cavalier", "paladin", "great knight", "archer", "sniper", "bow knight" ],
+    available_skills: [ "dual strike+", "charm", "aether", "rightful king", "discipline", "outdoor fighter", "defender", "aegis", "luna", "dual guard+", "skill+2", "prescience", "hit rate+20", "bowfaire", "rally skill", "bowbreaker"]
   },
   lissa: {
     base_id: 3,
     name: "Lissa",
     starting_class: "cleric",
-    available_classes: [ "cleric", "war cleric", "sage", "pegasus knight", "falcon knight", "dark flier", "troubadour", "valkyrie" ]
+    available_classes: [ "cleric", "war cleric", "sage", "pegasus knight", "falcon knight", "dark flier", "troubadour", "valkyrie" ],
+    available_skills: [ "miracle", "healtouch", "rally luck", "renewal", "rally magic", "tomefaire", "speed+2", "relief", "rally speed", "lancefaire", "rally movement", "galeforce", "resistance+2", "demoiselle", "rally resistance", "dual support+"] 
   },
   frederick: {
     base_id: 4,
     name: "Frederick",
     starting_class: "great knight",
-    available_classes: [ "cavalier", "paladin", "knight", "general", "great knight", "wyvern rider", "wyvern lord", "griffon rider" ]
+    available_classes: [ "cavalier", "paladin", "knight", "general", "great knight", "wyvern rider", "wyvern lord", "griffon rider" ],
+    available_skills: [ "discipline", "outdoor fighter", "defender", "aegis", "defense+2", "indoor fighter", "rally defense", "pavise", "luna", "dual guard+", "strength+2", "tantivy", "quick burn", "swordbreaker", "deliverer", "lancebreaker"]
   }
 };
 export default characters;   
