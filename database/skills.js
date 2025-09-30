bc486254d811bd93afef3f4366f8bc2eb3be6319
dimitri3 <157725336+dimitri3-ctr@users.noreply.github.com>
2025-09-29 20:50:23 -0500

    Delete database/skills.js

--- database/skills.js
+++
@@ -1,8 +0,0 @@
-const class_skills = {
-  "lord": [ "Dual Strike+", "Charm" ],
-  "great lord": [ "Aether", "Rightful King" ],
-  "tactician": [ "Veteran", "Solidarity" ],
-  "grandmaster": [ "Ignis", "Rally Spectrum" ]
-};
-export default class_skills;
-  
