cc46261635f5e2e4daa4a0aec226018dad7cc521
dimitri3 <157725336+dimitri3-ctr@users.noreply.github.com>
2025-09-29 19:34:56 -0500

    Update main.js

--- database/main.js
+++ database/main.js
@@ -1,9 +1,9 @@
 import characters from "./characters.js";
-import skillData from "./skills.js";
-const lordSkills = skillData.class_skills["lord"];
-const greatlordSkills = skillData.class_skills["great lord"];
-const tacticianSkills = skillData.class_skills["tactician"];
-const grandmasterSkills = skillData.class_skills["grandmaster"];
-const avatarmClasses = characterData.
+import class_skills from "./skills.js";
+const lordSkills = class_skills["lord"];
+const greatlordSkills = class_skills["great lord"];
+const tacticianSkills = class_skills["tactician"];
+const grandmasterSkills = class_skills["grandmaster"];
+const avatarMClasses = characters.avatarM.available_classes;
 
   
